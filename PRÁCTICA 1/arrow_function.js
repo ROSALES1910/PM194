a785cc4c0b5d8b9422b1842444b14daadf637fe2
KARLA MARÍA <kmaria.grosales@gmail.com>
2025-05-16 19:50:32 -0600

    arrow_funtion.js

---
+++ PRÁCTICA 1/arrow_function.js
@@ -0,0 +1,6 @@
+const cuadrado = numero => numero * numero;
+
+// muestra en consola probando con tres números diferentes
+console.log(cuadrado(2));
+console.log(cuadrado(5));
+console.log(cuadrado(10));
