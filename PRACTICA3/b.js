8e089a20e7d81937f404e1ad0358359fa848b2ea
KARLA MARÍA <kmaria.grosales@gmail.com>
2025-05-22 14:39:43 -0600

    b

--- PRACTICA3/b.js
+++ PRACTICA3/b.js
@@ -1,6 +1,18 @@
-function verificarUsuario(usuario){
-    //retorna una persona aquí
+function verificarUsuario(usuario) {
+    return new Promise((resolve, reject) => {
+        if (usuario === "admin") {
+            resolve("Acceso concedido");
+        } else {
+            reject("Acceso denegado");
+        }
+    });
 }
 
-//usa .then() y .catch() para manejar el resultado
+// usa .then() y .catch() para manejar el resultado
 verificarUsuario("admin")
+    .then(res => console.log(res)) // acceso concedido
+    .catch(err => console.error(err));
+
+verificarUsuario("Ivan")
+    .then(res => console.log(res))
+    .catch(err => console.error(err)); // acceso denegado
