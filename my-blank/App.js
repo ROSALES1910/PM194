d54849b42905947ba3164041384dedb99d3805af
KARLA MARÍA <kmaria.grosales@gmail.com>
2025-06-10 17:15:42 -0600

    Práctica 4 (primera parte)

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,15 +1,29 @@
+/* ZONA 1:  IMPORTACIONES*/
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
+const Texto = (props) => {
+  const {contenido} = props;
+  // const { } = props; // Si necesitas destructurar props
+  return (
+    <Text> {contenido} </Text>
+  );
+};
+
+/*ZONA 2: MAIN*/
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title = "TLABAJA!!"></Button>
+      <Texto contenido = "HOLA"></Texto>
+      <Texto contenido = "MUNDO"></Texto>
+      <Texto contenido = "REACT NATIVE"></Texto>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+/*ZONA 3: ESTÉTICA DEL SCREEN*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
