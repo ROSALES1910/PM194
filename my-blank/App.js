50d1e86c33e6d7e7a506073669f6cf692d5aa57b
KARLA MARÍA <kmaria.grosales@gmail.com>
2025-06-13 20:08:18 -0600

    PRÁCTICA 5

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,46 +1,44 @@
-/* ZONA 1:  IMPORTACIONES*/
+/* ZONA 1: IMPORTACIONES */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import React, {useState} from 'react';
 
-
-const Boton = () => {
-   const [contenido, setContenido] = useState("Presiona el botón"); //se inicializa el estado del botón
-   const actualizaButton = () => {setContenido('ESTADO ACTUALIZADO DEL BOTÓN')} //se va mandar llamar cuando presionemos el botón
-    return (
-      <Button title = {contenido} onPress={actualizaButton}> {contenido} </Button>
-    )
-};
-
-const Texto = () => {
+const Texto = ({style}) => {
   const [contenido, setContenido] = useState("Hola Mundo RNative"); 
-  const actualizaTexto = () => {setContenido('ESTADO ACTUALIZADO DEL TEXT')} //se va mandar llamar cuando presionemos el botón
+  const actualizaTexto = () => {setContenido('ESTADO ACTUALIZADO DEL TEXT')}
   return (
     <View>
-      <Text onPress={actualizaTexto}>{contenido}</Text>
+      <Text style = {[styles.text, style]} onPress={actualizaTexto}>{contenido}</Text>
     </View>
   );
 };
 
-/*ZONA 2: MAIN*/
+/* ZONA2: MAIN */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Boton></Boton>
-      <Texto></Texto>
-      <Texto></Texto>
-      <Texto></Texto>
+      <Texto style = {styles.red}></Texto>
+      <Texto style = {styles.blue}></Texto>
+      <Texto style = {styles.green}></Texto>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-/*ZONA 3: ESTÉTICA DEL SCREEN*/
+/* ZONA 3: ESTILOS */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'baseline',
     justifyContent: 'center',
+    flexDirection: 'row',
   },
+  text:{
+    color:'white',
+    fontSize: 27,
+  },
+  red:{backgroundColor: 'red'},
+  blue:{backgroundColor: 'blue'},
+  green:{backgroundColor: 'green'}
 });
